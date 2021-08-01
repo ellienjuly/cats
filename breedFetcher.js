5837c38a7d86104eb77a3e00a048d99001f0d19b
Taeyeong Nam <ellienjuly@gmail.com>
2021-08-01 20:36:06 +0000

    Fetch cat description

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,20 +1,24 @@
 const request = require('request');
-let breed = process.argv.slice(2);
 
-let apiUrl = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
+const fetchBreedDescription = function(breedName, callback) {
+  
+  let url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  
+  request(url, (error, response) => {
+    if (!response) {
+      console.log("Error content:", error);
+      return error;
+    }
 
-request(apiUrl, (error, response) => {
- 
-  if (!response) {
-    console.log("Error content:", error);
-    return error;
-  }
+    const data = JSON.parse(response.body);
+    if (data.length < 1) {
+      console.log(`Breed '${breedName}' doesn't exit in the system`);
+      return;
+    } else {
+      callback(error, data[0].description);
+      return;
+    }
+  });
+};
 
-  const data = JSON.parse(response.body);
-  if (data.length < 1) {
-    console.log(`Breed '${breed}' doesn't exit in the system`);
-    return;
-  } else {
-    console.log(data[0].description);
-  }
-});
+module.exports = { fetchBreedDescription };
